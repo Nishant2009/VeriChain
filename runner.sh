1f39d46d661f1074cdd0f2246b268bdc2ac67b42
Nishant2009 <92501994+Nishant2009@users.noreply.github.com>
2024-12-12 18:23:07 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,10 +1,9 @@
-# Install cloudflared
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-sudo dpkg -i cloudflared.deb
-rm -rf cloudflared.deb log.txt
-
 # Start cloudflared tunnel
-sudo cloudflared service install eyJhIjoiYjM3MjBhOGFhMzU4YmY3NmFjZTE3MTg0ZGY0YmIwMmIiLCJ0IjoiMzNmYTc1Y2EtNDM3ZS00MjFiLTlmZjYtMTJhMTBhZjc2YzUxIiwicyI6Ik16UTRZVGxsTm1FdFpqa3pZeTAwWmpFeExUbG1aamt0TkRZeE1ETTBORGxrT1RCaSJ9
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiYjM3MjBhOGFhMzU4YmY3NmFjZTE3MTg0ZGY0YmIwMmIiLCJ0IjoiMzNmYTc1Y2EtNDM3ZS00MjFiLTlmZjYtMTJhMTBhZjc2YzUxIiwicyI6IllUYzJNbVV6T1RndFlUSTFOaTAwWlRabExUaGlOR010WW1WaU5UZGxabUZqT0dJeSJ9
 
 # Run the app
 gunicorn app:app
