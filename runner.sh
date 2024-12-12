09b2eab88557eb5a06d77023b12ce9eed87a1cd0
Nishant2009 <92501994+Nishant2009@users.noreply.github.com>
2024-12-12 18:50:40 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,11 +1,13 @@
 # Install cloudflared
-sudo apt install cloudflared
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared.deb
+rm -rf cloudflared.deb
 
 cloudflared tunnel --url 127.0.0.1:8080 --logfile cloudflared_log.txt >/dev/null 2>&1 &
 
 
 # wait 5 seconds for cloudflared to start and get the URL
-sleep 5
+sleep 10
 cat cloudflared_log.txt | grep -o 'https://[-0-9a-z]*\.trycloudflare.com'
 
 # Start cloudflared tunnel
