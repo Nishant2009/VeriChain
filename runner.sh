86048936bc15b26a539ed4f66f3212b03b9f2563
Nishant2009 <92501994+Nishant2009@users.noreply.github.com>
2024-12-12 18:21:14 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,6 +6,5 @@ rm -rf cloudflared.deb log.txt
 # Start cloudflared tunnel
 sudo cloudflared service install eyJhIjoiYjM3MjBhOGFhMzU4YmY3NmFjZTE3MTg0ZGY0YmIwMmIiLCJ0IjoiMzNmYTc1Y2EtNDM3ZS00MjFiLTlmZjYtMTJhMTBhZjc2YzUxIiwicyI6Ik16UTRZVGxsTm1FdFpqa3pZeTAwWmpFeExUbG1aamt0TkRZeE1ETTBORGxrT1RCaSJ9
 
-
 # Run the app
 gunicorn app:app
